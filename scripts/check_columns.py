98a1237afafbece15c0d2f061138473090b47c59
Gourav180420 <Gourav.mishra@euruni.edu>
2025-06-10 15:03:26 +0200

    Describe your changes here

--- scripts/check_columns.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data/cleaned/anime_cleaned.csv")
-print("Columns in the CSV file:")
-print(df.columns.tolist())
--- scripts/config_example.py
+++
@@ -1,5 +0,0 @@
-# config_example.py
-DB_HOST = 'localhost'
-DB_USER = 'your_username'
-DB_PASSWORD = 'your_password'
-DB_NAME = 'anime_db'
